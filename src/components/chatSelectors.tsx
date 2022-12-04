694c38f184695c627ba8b1baa18947f2c851336a
Victor-Mannelli <imp.mannelli@gmail.com>
2022-12-04 08:00:38 -0300

    refactor: identation and h1

--- src/components/chatSelectors.tsx
+++ src/components/chatSelectors.tsx
@@ -2,7 +2,7 @@
 export default function ChatSelectors(): JSX.Element {
 	return (
 		<div className="bg-second-color-palette w-48 min-h-screen pt-[3.75rem]">
-            <h1 className="text-light-gray p-3"> asd </h1>
+            <h1 className="text-light-gray p-3"> SELETORES DE CHAT </h1>
 		</div>
 	);
 }
